fd8a9ccf7173ff042150b60294b43cd1c5ddfe8c
j3ffh95 <j3ffh95@gmail.com>
2020-08-01 20:24:18 -0400

    Created the DOM Elements

--- main.js
+++ main.js
@@ -2,3 +2,11 @@
 let time = 5,
   score = 0,
   isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector("#word-input"),
+  currentWord = document.querySelector("#current-word"),
+  scoreDisplay = document.querySelector("#score"),
+  timeDisplay = document.querySelector("#time"),
+  message = document.querySelector("#message"),
+  seconds = document.querySelector("#seconds");
